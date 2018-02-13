52110cc2ee5aea9655b9d41be708eb473dc9a2d1
Forbes Lindesay <forbes@lindesay.co.uk>
2018-02-13 17:44:54 +0000

    Remove slightly pointless `Dialog` export from react-modal
    The built in `dialog` element supports the `open` property with the latest react typescript.

--- packages/react-modal/src/index.tsx
+++ packages/react-modal/src/index.tsx
@@ -1,11 +1,9 @@
-import Dialog from './Dialog';
 import ModalDialog, {ModalDialogProps} from './ModalDialog';
 import ModalProvider, {
   ModalProviderProps,
   ModalRenderProps,
 } from './ModalProvider';
 
-export {Dialog};
 export {ModalDialogProps};
 export default ModalDialog;
 
